1adb709f05337b8ff75b5f83dc8a9930f8b15222
Abdul Aziz Mubarak <abdulazizmubarak1@gmail.com>
2023-08-26 15:27:47 +0800

    pindakan isi class deklarasi ke main class

--- hydroponic-auto/hydroponic-auto.ino
+++ hydroponic-auto/hydroponic-auto.ino
@@ -1,4 +1,32 @@
-#include "deklarasi.h"
+// #include "deklarasi.h"
+// insialisasi Modul SD Card
+#include <SPI.h>
+#include <SD.h>
+
+// Inisialisasi Pin RTC
+#include <Wire.h>
+#include <RTClib.h>
+
+
+// Inisilaisasi LCD Display
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+// Import library EEPROM
+#include <EEPROM.h>
+
+// Inisialisasi PIN Sensor pH Meter
+#define PH_PIN 34 // Pin GPIO36
+float voltage, phValue;
+
+// inisialisasi pin sensor TDS
+#define TDS_PIN 35
+#define VREF 3.0      // tegangan referensi analog (Volt) dari ADC
+#define SCOUNT  10           // jumlah titik sampel
+int analogBuffer[SCOUNT];    // menyimpan nilai analog dalam array, membaca dari ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0,copyIndex = 0;
+float averageVoltage = 0, tdsValue = 0, temperature = 25;
 
 #include <ThingerESP32.h>
 #include "arduino_secret.h"
@@ -57,12 +85,13 @@ void setup() {
   thing.add_wifi(SSID, SSID_PASSWORD);
 
   //mulai koneksi ke Thinger.io
-  thing["PH"] >> phValue;    // nilai pH yang akan dikirim
-  thing["TDS"] >> tdsValue;  // nilai TDS yang akan dikirim
-  thing.start();
+  thing["PH"] >> inputValue(phValue);    // nilai pH yang akan dikirim
+  thing["TDS"] >> inputValue(tdsValue);  // nilai TDS yang akan dikirim
+  // thing.start();
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
   // Baca waktu dari RTC
   DateTime now = rtc.now();
   String waktuSekarang = String(now.day(), DEC) + "-"
